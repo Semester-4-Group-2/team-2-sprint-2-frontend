c68a44e449d21ecf5f06dd0fc4fdeed99560f87e
Chris Pinto <Chris.pinto1234@gmail.com>
2023-04-12 15:11:26 -0230

    added news

--- team2-react/src/Components/Home.jsx
+++ team2-react/src/Components/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Carousel from "react-elastic-carousel";
 
+import "./styles.css";
+
 export default function Home() {
   const breakPoints = [
     { width: 1, itemsToShow: 1 },
@@ -21,23 +23,35 @@ export default function Home() {
 
   return (
     <div className="App">
+  <div className="latest-news">
+    <h2>Latest News and Updates</h2>
+    <div className="article">
+      <img src="./images/test1.png" alt="News 1"  />
+      <h3>New Game Release: Cyberpunk 2077</h3>
+      <p>Get ready to dive into the futuristic world of Night City with Cyberpunk 2077, the latest game from CD Projekt Red. Explore the massive open world, customize your character, and make choices that will shape the story.</p>
+    </div>
+    <div className="article">
+      <img src="./images/test2.png" alt="News 2" />
+      <h3>Upcoming DLC: Assassin's Creed Valhalla</h3>
+      <p>Fans of Assassin's Creed Valhalla have a lot to look forward to with the upcoming Wrath of the Druids DLC. Set in Ireland, this new expansion promises new characters, new quests, and plenty of challenges.</p>
+    </div>
+    <div className="article">
+      <img src="./images/test3.png" alt="News 3" />
+      <h3>Multiplayer Update: Red Dead Redemption 2</h3>
+      <p>It's been a long time coming, but Red Dead Redemption 2 players can finally enjoy the game's multiplayer mode. With a variety of activities to choose from, including co-op missions and player-vs-player modes.</p>
+    </div>
+    
+  </div>
       <h1>Top Sellers This Week</h1>
       <Carousel breakPoints={breakPoints}>
         {images.map((image, index) => (
           <img key={index} src={image.src} alt={image.alt} className={image.className} style={image.style} />
         ))}
       </Carousel>
-      <style>
-        {`
-          .carousel-image {
-            max-width: 100%;
-            height: auto;
-          }
-        `}
-      </style>
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Home />, rootElement);
+
