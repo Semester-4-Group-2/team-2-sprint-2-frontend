1a3a3dfb1c21e9c4d0b0f401506eeda13778ccff
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2023-04-12 11:41:34 -0230

    Update VideoGames.jsx

--- team2-react/src/Components/VideoGames.jsx
+++ team2-react/src/Components/VideoGames.jsx
@@ -4,10 +4,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function VideoGames() {
-    const handleSubmit = (e) => e.preventDefault()
+  const handleSubmit = (e) => e.preventDefault();
   return (
-
     <main>
+      <header>
+        <form className="search" onSubmit={handleSubmit}>
+          <input className="search__input" type="text" id="search" />
+          <button className="search__button" type="submit">
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </form>
+      </header>
       Select which function.
       <nav>
         <Link to="/videogames/getAll" className="links">
@@ -15,15 +22,5 @@ export default function VideoGames() {
         </Link>
       </nav>
     </main>
-
-    <header>
-      <form className="search" onSubmit={handleSubmit}>
-        <input className="search__input" type="text" id="search" />
-        <button className="search__button" type="submit">
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </button>
-      </form>
-    </header>
-
   );
 }
