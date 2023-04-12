f607b6d0df9066b1d725695cdb024d14bbac5066
Chris Pinto <Chris.pinto1234@gmail.com>
2023-04-12 15:23:22 -0230

    removed icons

--- team2-react/src/Components/VideoGames.jsx
+++ team2-react/src/Components/VideoGames.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function VideoGames() {
     const handleSubmit = (e) => e.preventDefault()
@@ -10,7 +10,8 @@ export default function VideoGames() {
       <form className="search" onSubmit={handleSubmit}>
         <input className="search__input" type="text" id="search" />
         <button className="search__button" type="submit">
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        Submit
+          {/* <FontAwesomeIcon icon={faMagnifyingGlass} /> */}
         </button>
       </form>
     </header>
