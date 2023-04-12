c16273c6f377dd7554640c18de119cbdc33f6a8a
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2023-04-12 11:51:48 -0230

    Working

--- team2-react/src/App.jsx
+++ team2-react/src/App.jsx
@@ -10,56 +10,36 @@ import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import VideoGames from "./Components/VideoGames";
 import Navbar from "./Components/Navbar";
-import styled from "styled-components";
-
-const Container = styled.div`
-  height: 100vh;
-  scroll-snap-type: y mandatory;
-  scollintoview: smooth;
-  scroll-behavior: smooth;
-  overflow-y: scroll;
-  scrollbar-width: none;
-  padding-top: 60px; /* Add top padding to account for the navbar */
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  @media only screen and (max-width: 768px) {
-    scroll-snap-type: none;
-  }
-`;
 
 function App() {
   return (
-    <Container>
-      <Router>
-        <Navbar />
-        <header>
-          <h1>Final Sprint, Team 2</h1>
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/videogames" element={<VideoGames />} />
-          <Route path="/videogames/getAll" element={<FindAll />} />
-          {/* confirm search specific */}
-          <Route path="/videogames/getById/{id}" element={<FindByID />} />
-          <Route path="/videogames/getByName/{name}" element={<FindByName />} />
-          <Route
-            path="/videogames/getByGenre/{genre}"
-            element={<FindByGenre />}
-          />
-          <Route
-            path="/videogames/getByDevelopers/{developers}"
-            element={<FindByDev />}
-          />
-          <Route
-            path="/videogames/getByPlatform?platform={platform}"
-            element={<FindByPlatform />}
-          />
-        </Routes>
-        <Footer />
-      </Router>
-    </Container>
+    <Router>
+      <header>
+        <h1>Final Sprint, Team 2</h1>
+        {/* <Navbar /> */}
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videogames" element={<VideoGames />} />
+        <Route path="/videogames/getAll" element={<FindAll />} />
+        {/* confirm search specific */}
+        <Route path="/videogames/getById/{id}" element={<FindByID />} />
+        <Route path="/videogames/getByName/{name}" element={<FindByName />} />
+        <Route
+          path="/videogames/getByGenre/{genre}"
+          element={<FindByGenre />}
+        />
+        <Route
+          path="/videogames/getByDevelopers/{developers}"
+          element={<FindByDev />}
+        />
+        <Route
+          path="/videogames/getByPlatform?platform={platform}"
+          element={<FindByPlatform />}
+        />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
--- team2-react/src/Components/VideoGames.jsx
+++ team2-react/src/Components/VideoGames.jsx
@@ -1,21 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function VideoGames() {
-  const handleSubmit = (e) => e.preventDefault();
   return (
     <main>
-      <header>
-        <form className="search" onSubmit={handleSubmit}>
-          <input className="search__input" type="text" id="search" />
-          <button className="search__button" type="submit">
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
-        </form>
-      </header>
-      Select which function.
+      <h1>Select which function.</h1>
       <nav>
         <Link to="/videogames/getAll" className="links">
           Get Full List
