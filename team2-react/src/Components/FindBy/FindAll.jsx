cce91a8df5eb96abf48e5b801786659aa435076d
Mark <markhannem3@gmail.com>
2023-04-12 13:54:12 -0230

    Table for GetAllGamesList

--- team2-react/src/Components/FindBy/FindAll.jsx
+++ team2-react/src/Components/FindBy/FindAll.jsx
@@ -1,4 +1,61 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+const Title = styled.h2`
+  font-size: 28px;
+  margin-bottom: 20px;
+`;
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+`;
+
+const Th = styled.th`
+  padding: 10px;
+  text-align: left;
+  font-size: 18px;
+  font-weight: bold;
+  background-color: #f2f2f2;
+`;
+
+const Td = styled.td`
+  padding: 10px;
+  text-align: left;
+`;
+
+const EvenRow = styled.tr`
+  background-color: #f9f9f9;
+  &:hover,
+  &:focus {
+    background-color: #f2f2f2;
+    cursor: pointer;
+  }
+`;
+
+const OddRow = styled.tr`
+  &:hover,
+  &:focus {
+    background-color: #f2f2f2;
+    cursor: pointer;
+  }
+`;
+
+const TitleCell = styled.td`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const PlatformsCell = styled.td`
+  font-size: 16px;
+`;
 
 export default function FindAll() {
   const [videoGames, setVideoGames] = React.useState([]);
@@ -9,21 +66,39 @@ export default function FindAll() {
         setVideoGames(result);
       });
   }, []);
+
   return (
-    <div>
-      <h2>List of All Video Games</h2>
-      {videoGames.map((videoGame) => (
-        <div>
-          Name: {videoGame.name}
-          <br />
-          Developer(s): {videoGame.developers}
-          <br />
-          Platform(s): {videoGame.platforms}
-          <br />
-          Genre: {videoGame.genre}
-          <br />
-        </div>
-      ))}
-    </div>
+    <Container>
+      <Title>List of All Video Games</Title>
+      <Table>
+        <thead>
+          <tr>
+            <Th>Name</Th>
+            <Th>Developer(s)</Th>
+            <Th>Platform(s)</Th>
+            <Th>Genre</Th>
+          </tr>
+        </thead>
+        <tbody>
+          {videoGames.map((videoGame, index) =>
+            index % 2 === 0 ? (
+              <EvenRow key={index}>
+                <TitleCell>{videoGame.name}</TitleCell>
+                <Td>{videoGame.developers}</Td>
+                <PlatformsCell>{videoGame.platforms}</PlatformsCell>
+                <Td>{videoGame.genre}</Td>
+              </EvenRow>
+            ) : (
+              <OddRow key={index}>
+                <TitleCell>{videoGame.name}</TitleCell>
+                <Td>{videoGame.developers}</Td>
+                <PlatformsCell>{videoGame.platforms}</PlatformsCell>
+                <Td>{videoGame.genre}</Td>
+              </OddRow>
+            )
+          )}
+        </tbody>
+      </Table>
+    </Container>
   );
 }
