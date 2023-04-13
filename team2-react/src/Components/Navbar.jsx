8f78c4dd360ad5c4f34ed9b1c17aa42f6f2b2dd5
Chris Pinto <Chris.pinto1234@gmail.com>
2023-04-13 09:45:13 -0230

    changed navbar

--- team2-react/src/Components/Navbar.jsx
+++ team2-react/src/Components/Navbar.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap');
-</style>
 
 const Section = styled.div`
   display: flex;
   justify-content: center;
-  background-color: #3e8e41 ;
+  background-color: #3e8e41;
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 60px;
   z-index: 100;
-
-  @media only screen and (max-width: 768px) {
-    width: 100%;
-    scroll-snap-align: none;
-  }
 `;
 
 const Container = styled.div`
@@ -31,14 +23,26 @@ const Container = styled.div`
 
   @media only screen and (max-width: 768px) {
     width: 100%;
-     scroll-snap-align: none;
+    padding: 10px;
+  }
+`;
+
+const Logo = styled(Link)`
+  font-size: 24px;
+  font-weight: bold;
+  color: white;
+  text-decoration: none;
+  margin-right: 50px;
+
+  @media only screen and (max-width: 768px) {
+    margin-right: 0;
   }
 `;
 
 const Links = styled.div`
   display: flex;
   align-items: center;
-  height: 100%; /* Set a fixed height */
+  height: 100%;
 
   & a {
     color: white;
@@ -46,8 +50,8 @@ const Links = styled.div`
     font-size: 20px;
     padding: 25px;
     line-height: 20px; /* Use line-height instead of padding */
+    border-bottom: none; /* Remove the underline */
   }
-
   & a:hover {
     color: black;
     background-color: white;
@@ -55,16 +59,25 @@ const Links = styled.div`
   }
 `;
 
-
-
-
 const List = styled.ul`
   display: flex;
   gap: 20px;
   list-style: none;
+  margin: 0;
 
   @media only screen and (max-width: 768px) {
     display: none;
+  }
+`;
+
+const MenuIcon = styled.div`
+  display: none;
+  cursor: pointer;
+
+  @media only screen and (max-width: 768px) {
+    display: block;
+    font-size: 24px;
+    color: white;
   }
 `;
 
@@ -72,16 +85,30 @@ const Navbar = () => {
   return (
     <Section>
       <Container>
+        <Logo to="/">GameGo</Logo>
+        <MenuIcon>☰</MenuIcon>
         <Links>
           <List>
-            <Link to="/" className="links">
-              Home
-            </Link>
-            <Link to="/videogames" className="links">
-              Video Games
-            </Link>
-            <Link to="/binarysearch" className="links">
-            </Link>
+            <li>
+              <Link to="/" className="links">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/videogames" className="links">
+                Video Games
+              </Link>
+            </li>
+            <li>
+              <Link to="/binarysearch" className="links">
+                Sign Up
+              </Link>
+            </li>
+            <li>
+              <Link to="/binarysearch" className="links">
+                Sign In
+              </Link>
+            </li>
           </List>
         </Links>
       </Container>
@@ -90,3 +117,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
