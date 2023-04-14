c606f50a08c8cc95c364d9082245192b1c6a168b
Mark <markhannem3@gmail.com>
2023-04-14 13:23:02 -0230

    FindByName style complete

--- team2-react/src/Components/FindBy/FindByName.jsx
+++ team2-react/src/Components/FindBy/FindByName.jsx
@@ -1,5 +1,83 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const FindByNameWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  background-color: #f1f1f1;
+`;
+
+const PrevButton = styled.button`
+  display: inline-block;
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  color: #fff;
+  background-color: #570b83;
+  border: none;
+  border-radius: 3px;
+  text-decoration: none;
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: #8d24aa;
+    transform: translateY(-2px);
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const FindByNameHeading = styled.h2`
+  margin-bottom: 1rem;
+`;
+
+const FindByNameInput = styled.input`
+  padding: 0.5rem 1rem;
+  border-radius: 3px;
+  margin-right: 1rem;
+`;
+
+const FindByNameSearchButton = styled.button`
+  display: inline-block;
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  color: #fff;
+  background-color: #570b83;
+  border: none;
+  border-radius: 3px;
+  text-decoration: none;
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: #8d24aa;
+    transform: translateY(-2px);
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const TableWrapper = styled.div`
+  margin-top: 2rem;
+  width: 30%;
+`;
+
+const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+`;
+
+const Th = styled.th`
+  background-color: #570b83;
+  color: #fff;
+  font-weight: bold;
+  padding: 0.5rem;
+  text-align: left;
+`;
+
+const Td = styled.td`
+  border: 1px solid #ddd;
+  padding: 0.5rem;
+`;
 
 export default function FindByName() {
   const [name, setName] = useState("");
@@ -10,46 +88,51 @@ export default function FindByName() {
       .then((res) => res.json())
       .then((result) => {
         setVideoGames(result);
-
-        return videoGames;
       });
-  }, [name, videoGames]);
+  }, [name]);
 
   const handleGoBack = () => {
     window.history.back();
   };
 
   return (
-    <div>
-      <button onClick={handleGoBack}>Previous Page</button>
-      <h2>Find Video Games by Name</h2>
-      <input
-        type="text"
-        name="name-input"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-
-      {videoGames.map((videoGame) => (
-        <div>
-          <Link
-            to="/videogames/game"
-            state={{
-              id: videoGame.id,
-            }}
-          >
-            Name: {videoGame.name}
-          </Link>
-          <br />
-          Developer(s): {videoGame.developers}
-          <br />
-          Platform(s): {videoGame.platforms}
-          <br />
-          Genre: {videoGame.genre}
-          <br />
-          <br />
-        </div>
-      ))}
-    </div>
+    <FindByNameWrapper>
+      <PrevButton onClick={handleGoBack}>Previous Page</PrevButton>
+      <FindByNameHeading>Search for Video Games by Name</FindByNameHeading>
+      <div>
+        <label htmlFor="name-input">Name:</label>
+        <FindByNameInput
+          type="text"
+          id="name-input"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <FindByNameSearchButton onClick={() => {}}>
+          Search
+        </FindByNameSearchButton>
+      </div>
+      {videoGames.length > 0 && (
+        <TableWrapper>
+          <Table>
+            <thead>
+              <tr>
+                <Th>Name</Th>
+                <Th>Platform</Th>
+                <Th>Genre</Th>
+              </tr>
+            </thead>
+            <tbody>
+              {videoGames.map((game) => (
+                <tr key={game.id}>
+                  <Td>{game.name}</Td>
+                  <Td>{game.platforms}</Td>
+                  <Td>{game.genre}</Td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </TableWrapper>
+      )}
+    </FindByNameWrapper>
   );
 }
